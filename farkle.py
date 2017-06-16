69b93e0f59eb4e546d0a20e8425252ba29848d70
Shane Newbill <snewbill@gmail.com>
2017-06-16 09:02:54 -0600

    New GitKraken install.  Up to date with project.

--- farkle.py
+++ farkle.py
@@ -1,55 +1,70 @@
 import random
+import collections
+
 
 def randRoll():
     return random.randrange(1, 6)
 
 
 def rollXDice(x):
-    dieDict = {}
+    die_dict = {}
     for y in range(x):
-        dieDict[y] = randRoll()
-    return dieDict
+        die_dict[y] = randRoll()
+    return die_dict
 
 
 # Build out my combo definitions
 def isStraight(x):
-   if(sorted(x.values()) == [1, 2, 3, 4, 5, 6]):
-      print("winner")
-      return(True)
+    if sorted(x.values()) == [1, 2, 3, 4, 5, 6]:
+        return True
+
 
 def isSixOfAKind(x):
     for i in range(6):
-        if(sorted(x.values()) == [i, i, i, i, i, i]):
-            print(i)
-            print("was the answer")
+        if sorted(x.values()) == [i, i, i, i, i, i]:
+            return True
+
 
 def isThreePair(x):
-    
+    result_list = [0] * 6
+    for i in list(x.values()):
+        result_list[(list(x.values())[i]) - 1] += 1
+    if result_list.__contains__(6):
+        return True
+    if result_list.__contains__(4) && result_list.__contains__(2):
+        return True
+    print(result_list)
 
 
-
-
-#def checkForScore(savedDice):
-    #savedDice is a dictionary
-    
-
-
-goat = {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1}
-
+"""
 for num in range(10000):
     qq = rollXDice(6)
     if(isStraight(qq)):
         print("got here")
-        quit
+#        break
     if(isSixOfAKind(qq)):
         print("and here")
-        quit
+#        break
+    if(isThreePair(qq)):
+        print("Confirmed.  Three Pair.")
+
+"""
 
 # Testing below.  Build a dict, then use it.
 
 goofball = {1: 1, 2: 3, 3: 2, 4: 4, 5: 5, 6: 6}
-if(isStraight(goofball)):
-    print("provided")
+# if(isStraight(goofball)):
+#    print("provided")
 
-if(isSixOfAKind(goat)):
-    print("got to goat")
+goat = {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1}
+# if(isSixOfAKind(goat)):
+#    print("got to goat")
+
+yAak = {1: 1, 2: 1, 3: 2, 4: 3, 5: 2, 6: 3}
+
+if isThreePair(yAak):
+    print("Three Pair Confirmed")
+
+print(type(list(yAak.values())) is list)
+# if(isThreePair(yAak)):
+#    print("Is Three Pair")

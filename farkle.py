7f1f3f15736929ac1fc7d8e6b1ceb2482ac9822f
Shane Newbill <snewbill@gmail.com>
2017-06-20 11:14:46 -0600

    Mid refactor.  Pushing to be safe.

--- farkle.py
+++ farkle.py
@@ -12,30 +12,59 @@ def roll_x_dice(x):
         die_dict[y] = rand_roll()
     return die_dict
 
+def check_combos(score, die_list):
+    if die_list == [0, 0, 0, 0, 0, 0]:
+        return score
+    is_straight(score, die_list)
+    is_three_pair(score, die_list)
+    is_ace(score, die_list)
+    is_five(score, die_list)
+
+
+def list_index(die_list, index):
+    ret_list = die_list
+    ret_list[index] = 0
+    return ret_list
+
 
 # Build out my combo definitions
 # Return value of combo.
-def is_straight(x, y):
-    if sorted(x.values()) == [1, 2, 3, 4, 5, 6]:
-        return null, 1500 + y
+def is_straight(score, die_list):
+    if sorted(die_list.values()) == [1, 2, 3, 4, 5, 6]:
+        return 1500 + score, [0, 0, 0, 0, 0, 0]
 
 
-def is_three_pair(x):
-    result_list = [0] * 6
+def is_three_pair(score, die_list):
     three_pair = [0, 0, 0, 2, 2, 2]
-    for i in list(x.values()):
-        result_list[(list(x.values())[i]) - 1] += 1
-    if result_list.__contains__(6):
-        return 1500
-    if result_list.__contains__(4) and result_list.__contains__(2):
-        return 1500
-    if sorted(result_list) == three_pair:
-        return 1500
+    if die_list.__contains__(6):
+        return 1500 + score, [0, 0, 0, 0, 0, 0]
+    elif die_list.__contains__(4) and die_list.__contains__(2):
+        return 1500 + score, [0, 0, 0, 0, 0, 0]
+    elif sorted(die_list) == three_pair:
+        return 1500 + score, [0, 0, 0, 0, 0, 0]
     else:
-        return -1
+        return score, [0, 0, 0, 0, 0, 0]
 
 
-def is_ace(x)
+def is_set(score, die_list):
+    result_list = [0] * 6
+    if result_list.__contains__(3):
+        return check_combos((result_list.index(3)+1)*100 + score, list_index(die_list, result_list.index(3)))
+    if result_list.__contains__(4):
+        return check_combos((result_list.index(4)+1)*100*2 + score, list_index(die_list, result_list.index(4)))
+    if result_list.__contains__(5):
+        return check_combos((result_list.index(5)+1)*100*3 + score, list_index(die_list, result_list.index(5)))
+    if result_list.__contains__(6):
+        return (result_list.index(6)+1)*100*4 + score, list_index(die_list, result_list.index(6))
+
+
+def is_ace(score, die_list):
+    if '1' in die_list.values():
+        return check_combos()
+
+
+def is_five(score, die_list):
+
 
 
 '''
@@ -60,6 +89,12 @@ for num in range(10000):
 
 """
 
+
+'''
+Might need this for sending selected dice into processor
+    for i in list(die_list.values()):
+        result_list[(list(die_list.values())[i]) - 1] += 1
+'''
 # Testing below.  Build a dict, then use it.
 
 goofball = {1: 1, 2: 3, 3: 2, 4: 4, 5: 5, 6: 6}

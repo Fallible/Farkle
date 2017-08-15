527e559fb768e44df6ebe5d88a791a17d4e8611f
Shane Newbill <snewbill@gmail.com>
2017-08-15 15:15:37 -0600

    Refactor dice_dict
    Changed the formatting of the initial dictionary.  This required changes to all functions that consume that dictionary.  Basically everything.  Still underway at this push.

--- farkle.py
+++ farkle.py
@@ -9,50 +9,50 @@ def rand_roll():
 def roll_x_dice(x):
     die_dict = {}
     for y in range(x):
-        die_dict[y] = rand_roll()
+        roll_value = rand_roll()
+        die_dict[roll_value] += 1
     return die_dict
 
+
 def check_combos(score, die_dict):
+    current_best = score, die_dict
     if list(die_dict.values()) == [0, 0, 0, 0, 0, 0]:
         return score
-    is_straight(score, die_dict)
+    if current_best(score) < is_straight(score, die_dict):
+        current_best
     is_three_pair(score, die_dict)
-    is_ace(score, die_dict)
-    is_five(score, die_dict)
+    #is_ace(score, die_dict)
+    #is_five(score, die_dict)
     #is_no_score()
 
 
-def histogram(die_dict):
-    ret_histogram = {}
-    for
-
-
-def list_index(die_dict, index):
-    ret_list[6]
-    for value in list(die_dict.values()):
-        if value is not 0:
-            ret_list[value - 1] += 1
-    #ret_list[index] = 0  # I have no idea what I was trying to do with this...
-    return ret_list
+#def list_index(die_dict, index):
+#    ret_list[6]
+#    for value in list(die_dict.values()):
+#        if value is not 0:
+#            ret_list[value - 1] += 1
+#    #ret_list[index] = 0  # I have no idea what I was trying to do with this...
+#    return ret_list
 
 
 # Build out my combo definitions
 # Return value of combo.
 def is_straight(score, die_dict):
-    if sorted(die_dict.values()) == [1, 2, 3, 4, 5, 6]:
-        return 1500 + score, [0, 0, 0, 0, 0, 0]
+    if sorted(die_dict.values()) == [1, 1, 1, 1, 1, 1]:
+        return 1500 + score, {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
 
 
 def is_three_pair(score, die_dict):
     three_pair = [0, 0, 0, 2, 2, 2]
-    if die_dict.__contains__(6):
-        return 1500 + score, [0, 0, 0, 0, 0, 0]
-    elif die_list.__contains__(4) and die_list.__contains__(2):
-        return 1500 + score, [0, 0, 0, 0, 0, 0]
-    elif sorted(die_list) == three_pair:
-        return 1500 + score, [0, 0, 0, 0, 0, 0]
+    empty_dict = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
+    if 6 in die_dict.values():
+        return 1500 + score, empty_dict
+    elif 4 in die_dict.values() and 2 in die_dict.values():
+        return 1500 + score, empty_dict
+    elif sorted(list(die_dict.values())) == three_pair:
+        return 1500 + score, empty_dict
     else:
-        return score, [0, 0, 0, 0, 0, 0]
+        return score, die_dict
 
 
 def is_set(score, die_list):
@@ -66,12 +66,12 @@ def is_set(score, die_list):
         return (die_list.index(6)+1)*100*4 + score, list_index(die_list, die_list.index(6))
 
 
-def is_ace(score, die_list):
-    if '1' in die_list.values():
-        return check_combos()
+#def is_ace(score, die_list):
+#    if '1' in die_list.values():
+#        return check_combos()
 
 
-def is_five(score, die_list):
+#def is_five(score, die_list):
 
 
 
@@ -109,15 +109,18 @@ goofball = {1: 1, 2: 3, 3: 2, 4: 4, 5: 5, 6: 6}
 # if(is_straight(goofball)):
 #    print("provided")
 
-goat = {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1}
+goat = {1: 2, 2: 2, 3: 2, 4: 0, 5: 0, 6: 0}
 # if(is_six_of_kind(goat)):
 #    print("got to goat")
 
-yAak = {1: 1, 2: 1, 3: 2, 4: 3, 5: 2, 6: 3}
+yAak = {1: 2, 2: 2, 3: 2, 4: 0, 5: 0, 6: 0}
 
-print(is_three_pair(yAak))
+print(check_combos(0, yAak))
+
+print(is_three_pair(0, yAak))
+#print(sorted(yAak.values()))
 
 
-print(type(list(yAak.values())) is list)
+#print(type(list(yAak.values())) is list)
 # if(is_three_pair(yAak)):
 #    print("Is Three Pair")
